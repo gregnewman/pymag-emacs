0b23ac9c5c721f91212269c2ec54f75868e957b9
Greg Newman <greg@20seven.org>
2009-10-10 15:45:16 -0400

    cleanup

---
+++ init.el
@@ -0,0 +1,19 @@
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(progn (cd "~/.emacs.d/vendor")
+       (normal-top-level-add-subdirs-to-load-path))
+
+(defconst emacs-config-dir "~/.emacs.d/configs/" "")
+(setq load-path (cons "~/.emacs.d" load-path))
+(defun load-cfg-files (filelist)
+  (dolist (file filelist)
+    (load (expand-file-name
+           (concat emacs-config-dir file)))
+    (message "Loaded config file: %s" file)
+    ))
+
+(load-cfg-files '("init_python"
+		  "init_main"))
+
+(require 'auto-complete)
+(require 'pymacs)
+(require 'flymake)
